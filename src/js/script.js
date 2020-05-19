7866101fbc0c3b82d0f84578d2c6c562252f0919
Piotr <piotr.wach122@gmail.com>
2020-05-19 13:21:52 +0200

    Add method that counts price of product

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,8 @@
             thisProduct.renderInMenu();
             thisProduct.getElements();
             thisProduct.initAccordion;
+            thisProduct.initOrderForm;
+            thisProduct.processOrder;
 
             console.log('new Product', thisProduct);
         }
@@ -112,12 +114,65 @@
 
         initOrderForm {
             const thisProduct = this;
-            console.log(thisProduct);
+
+            thisProduct.form.addEventListener('submit', function (event) {
+                event.preventDefault();
+                thisProduct.processOrder();
+            });
+
+            for (let input of thisProduct.formInputs) {
+                input.addEventListener('change', function () {
+                    thisProduct.processOrder();
+                });
+            }
+
+            thisProduct.cartButton.addEventListener('click', function (event) {
+                event.preventDefault();
+                thisProduct.processOrder();
+            });
         }
 
         processOrder {
             const thisProduct = this;
-            console.log(thisProduct);
+
+            /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+
+            const formData = utils.serializeFormToObject(thisProduct.form);
+            console.log('formData', formData);
+
+            /* set variable price to equal thisProduct.data.price */
+
+            let price = thisProduct.data.price;
+
+            /* START LOOP: for each paramId in thisProduct.data.params */
+
+            for (let paramId in thisProduct.data.params) {
+
+                /* save the element in thisProduct.data.params with key paramId as const param */
+
+                // const param.getElementById(paramId);
+                const param = thisProduct.data.params.getElementById(paramId);
+
+                /* START LOOP: for each optionId in param.options */
+
+                for (let optionId in param.options) {
+
+                    /* save the element in param.options with key optionId as const option */
+
+                    const option = param.options.getElementById(optionId);
+                    const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+                    /* START IF: if option is selected and option is not default */
+                    if (optionSelected && !option.default) {
+
+                        /* add price of option to variable price */
+
+
+                    }
+                    /* END LOOP: for each optionId in param.options */
+                }
+                /* END LOOP: for each paramId in thisProduct.data.params */
+            }
+            /* set the contents of thisProduct.priceElem to be the value of variable price */
 
         }
 
